9a03eb77e1b672bfc9ddc0e64e22e1e523a57c7e
esthe <ko990727@hmail.com>
2021-07-22 10:36:04 +0900

    Edit activity_main2.xml Commit

---
+++ app/src/main/java/kr/hs/emirim/ko502804/project7_1/Mainactivity2.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.ko502804.project7_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mainactivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainactivity2);
+    }
+}
